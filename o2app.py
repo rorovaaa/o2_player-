fa8e6262dbfa8e85a12f419636d4d07ee8ab0850
rorovaaa <ggtop924@gmail.com>
2026-07-13 12:07:07 +0400

    Update o2app.py

--- o2app.py
+++ o2app.py
@@ -1,5 +1,21 @@
+import sys
+import os
 from user.menu import MENUSKA
-import os 
 import json
-menu = MENUSKA()
-menu.start_menu()
+
+def main():
+    if '--gui' in sys.argv:
+        from source.gui_test import RetroPlayer
+        from PyQt6.QtWidgets import QApplication
+        app = QApplication(sys.argv)
+        app.setStyle('Fusion')
+        player = RetroPlayer()
+        player.show()
+        sys.exit(app.exec())
+    else:
+        from user.menu import MENUSKA
+        menu = MENUSKA()
+        menu.start_menu()
+
+if __name__ == '__main__':
+    main()
